bf77c2f0650bd61fd5668692e3d2d2a51b521227
pohann <37322213+pohann@users.noreply.github.com>
2018-04-02 21:40:14 +0800

    Update Max Test.py
    correct the way of specifying constraints

--- Max Test.py
+++ Max Test.py
@@ -179,12 +179,12 @@ for i in range(output_list_length):
     output_list.append(1)
 """construsct the constraints with only two consumers, two goods and two factors"""
 cons = ("""total demand of good1=supply of good1"""
-        {'type': 'eq','fun' : lambda x:x[0]+x[2]=CESpf([x[8],x[9]])},
+        {'type': 'eq','fun' : lambda x:x[0]+x[2]-CESpf([x[8],x[9]])},
         """total demand of good2=supply of good2"""
-        {'type': 'eq','fun' : lambda x:x[1]+x[3]=CESpf([x[10],x[11]])},
+        {'type': 'eq','fun' : lambda x:x[1]+x[3]-CESpf([x[10],x[11]])},
         """total demand of factor1=supply of factor1"""
-        {'type': 'eq','fun' : lambda x:x[4]+x[6]=x[8]+x[10]},
+        {'type': 'eq','fun' : lambda x:x[4]+x[6]-x[8]-x[10]},
         """total demand of factor2=supply of factor2"""
-        {'type': 'eq','fun' : lambda x:x[5]+x[7]=x[9]+x[11]},
+        {'type': 'eq','fun' : lambda x:x[5]+x[7]-x[9]-x[11]},
          )
 res = scipy.optimize.minimize(consumption.welfare_max,output_list,constraints=cons,method="SLSQP")
