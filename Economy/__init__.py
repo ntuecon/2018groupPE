ac07c69e4e0b04b9a097c1b30f75d6fc6ed0b092
pohann <37322213+pohann@users.noreply.github.com>
2018-04-30 12:02:39 +0800

    Create __init__.py

---
+++ Economy/__init__.py
@@ -0,0 +1,32 @@
+"""
+Created on Mon Apr 02 16:34:45 2018
+"""
+"""
+@author: WeiJin,PoHan
+"""
+
+"""
+Set up the basic environment for the economy
+"""
+
+import numpy as np
+import random
+import scipy
+from Consumer import Consumer
+from producer import Producer
+
+class Economy(object):
+    """
+    'c'=number of consumers in the economy
+    'g'=number of goods produced in the economy
+    'f'=number of factors used in the production
+    """
+    def __init__(self,c,g,f):
+        self.c = c
+        self.g = g
+        slef.f = f
+    """
+    Calculate the equilibria base on the consumers' preferences and technology
+    """
+    def Equilibrium(self):
+        
