917e1b0dfee777dc35c3f566ecc3277f430bb960
OoiWeiJin <37322231+OoiWeiJin@users.noreply.github.com>
2018-05-03 06:58:02 +0800

    Add files via upload

--- Economy/Consumer.py
+++ Economy/Consumer.py
@@ -8,7 +8,7 @@ Created on Mon Apr 02 16:34:45 2018
 import numpy as np
 import random
 from function import Loop
-from function import Nested_Loop
+from function import Nested_Loop_C
 
 """
 Define the preferences of consumers
@@ -32,8 +32,8 @@ class Consumer(object):
         """
         Define variables that will be useful for list iterations
         """
-        consumption_length = c * g
-        factor_ss_length = c * f
+        consumption_length = self.c * self.g
+        factor_ss_length = self.c * self.f
         consumer_length = consumption_length +  factor_ss_length
         c_list = np.array(output_list[0:consumption_length])
         f_list = np.array(output_list[consumption_length:consumption_length + factor_ss_length])
@@ -42,9 +42,9 @@ class Consumer(object):
         """
         y = random.randint(1,5)
         s = round(random.random(),2)
-        A = Nested_Loop(c,g,2)
-        B = Loop(c,2)
-        T = Loop(f,2)
+        A = Nested_Loop_C(self.c,self.g,2)
+        B = Loop(self.c,2)
+        T = Loop(self.f,2)
         
         
         """
@@ -53,32 +53,32 @@ class Consumer(object):
         weighted_utility_subc = np.multiply((c_list ** y) , A)
         utility_subc = np.array([])
         for i in range(len(weighted_utility_subc)):
-            if (i == 0) or (i % g == 0 and i < g * c):
-                utility_subc = np.append(utility_subc,(np.sum(weighted_utility_subc[i:i + g])) ** ((1 - s) / y))
+            if (i == 0) or (i % self.g == 0 and i < self.g * self.c):
+                utility_subc = np.append(utility_subc,(np.sum(weighted_utility_subc[i:i + self.g])) ** ((1 - s) / y))
         
         """
         Caluculate the utility that comes from supplying factors
         """
         tweighted_utility_subf = np.array([])
         i = 0
-        while i < c:
+        while i < self.c:
             j = 0
-            while j < f:
-                tweighted_utility_subf = np.append(tweighted_utility_subf,((f_list[(i * f) + j] ** (1 + T[j])) / (1 + T[j])))
+            while j < self.f:
+                tweighted_utility_subf = np.append(tweighted_utility_subf,((f_list[(i * self.f) + j] ** (1 + T[j])) / (1 + T[j])))
                 j += 1
             i += 1
         
         weighted_utility_subf = np.array([])
         for i in range(len(tweighted_utility_subf)):
             if i == 0:
-                weighted_utility_subf = np.append(weighted_utility_subf,((tweighted_utility_subf[i:i + f]) * B[i]))
-            if i % total_factors == 0 and i < total_consumers * total_factors:
-                weighted_utility_subf = np.append(weighted_utility_subf,(tweighted_utility_subf[i:i + f]) * B[i / f])
+                weighted_utility_subf = np.append(weighted_utility_subf,((tweighted_utility_subf[i:i + self.f]) * B[i]))
+            if i % self.f == 0 and i < self.c * self.f:
+                weighted_utility_subf = np.append(weighted_utility_subf,(tweighted_utility_subf[i:i + self.f]) * B[i / self.f])
         
         utility_subf = np.array([])
         for i in range(len(weighted_utility_subf)):
-            if (i== 0) or (i % f == 0 and i < c * f):
-                utility_subf = np.append(utility_subf,(np.sum(weighted_utility_subf[i:i + f])))
+            if (i== 0) or (i % self.f == 0 and i < self.c * self.f):
+                utility_subf = np.append(utility_subf,(np.sum(weighted_utility_subf[i:i + self.f])))
                 
         """
         Calculate the total utility for each consumer in the economy
